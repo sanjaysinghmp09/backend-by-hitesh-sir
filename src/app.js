6be24d5d8d3d0320b0ea542c58bfc85dd0f818ba
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-02 21:58:10 +0530

    install cors and cookies parser and apply in the currant project

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ import express from "express"
 
 const app = express()
 
+app.use(cors())
+// it's work but if any setting related need in cors we can add more 
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+
 
 
 export {app}
+
+
