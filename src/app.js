3be5288d178e1d5ba6b3d6ec59d55c8b60711be1
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-03 21:35:01 +0530

    create app and apply cors

--- src/app.js
+++ src/app.js
@@ -6,8 +6,12 @@ app.use(cors())
 // it's work but if any setting related need in cors we can add more 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: process.env.CORS_ORIGIN ,
+    
 }))
+
+app.use(express.json({limit: "16kb"}))
+app.use()
 
 
 
