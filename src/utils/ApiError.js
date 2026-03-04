f7dc15a5cee7288ec731c3047f1d61514221cdfa
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-04 21:09:50 +0530

    create asyncHandler

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,18 +1,23 @@
 class ApiError extends Error {
-    constructor (
-        statusCode ,
-        message = "Sothing went wrong !!",
-        error = [],
-        statck = ""
+    constructor(
+        statusCode,
+        message = "Something went wrong!!",
+        errors = [],
+        stack = ""
     ){
-        // explain super speacilly
-       super(message)
-       this.statusCode = statusCode
-       this.data = null 
-    //    what happend in the this.data
-       this.message
-       this.success = false;
-       this.errors = errors
+        super(message)
 
+        this.statusCode = statusCode
+        this.data = null
+        this.success = false
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
+
+export { ApiError }
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,1 +1,13 @@
-class ApiResponse
+class ApiResponse {
+    constructor (statusCode , data , message = "Success") {
+        this.statusCode = statusCode 
+        this.data = data 
+        this.message = message 
+        this.success = statusCode < 400 
+    }
+}
+
+export {ApiResponse}
+
+
+// mdn statusCode responses - https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
