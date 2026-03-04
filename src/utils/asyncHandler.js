c627da5c23b08ee3896b0cf1bf52b3ac4cf99e67
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-04 08:27:21 +0530

    asyncHandler func and promise

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,21 @@
-const asyncHandler = ()=> {} ;
+// const asyncHandler = ()=> {} ;
+
 
 
 export {asyncHandler}
+// const asyncHandler = (fn) => () => {}
+
+// Some codebase use this try catch method and some codebase use promise method 
+
+// const asyncHandler = (func)=> async (req , res , next )=> {
+//     try {
+//         await func(res , req , next)
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false ,
+//             message : error.message
+//         })
+//     }
+// }
+
