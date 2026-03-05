09d7b29a903530f3f17687b519b4a6c3871907f7
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-05 07:59:26 +0530

    define user model

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import { use } from "react";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+  avatar: {
+    type: String, //cloudinary url
+    required: true,
+  },
+  coverImage: {
+    type: String, //cloudinary url
+  },
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "video",
+    },
+  ],
+  password: {
+    type : String,
+    required: [true , "Password id required"]
+  } ,
+  refreshToken : {
+    type: String 
+  }
+
+
+} ,
+{
+    timestamps: true ,
+}
+
+
+);
+
+export const User = mongoose.model("User", userSchema);
