dcb8c5b4d5e3bbf5462a35295a07abc10fe1dee6
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-07 22:02:09 +0530

    USER MODEL AND VIDEO MODEL ARE READY

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,69 +1,94 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypy from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypy from "bcrypt";
 import { use } from "react";
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
-    index: true,
-  },
-  avatar: {
-    type: String, //cloudinary url
-    required: true,
-  },
-  coverImage: {
-    type: String, //cloudinary url
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "video",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-  ],
-  password: {
-    type : String,
-    required: [true , "Password id required"]
-  } ,
-  refreshToken : {
-    type: String 
-  }
-
-} ,
-{
-    timestamps: true ,
-}
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password id required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
-userSchema.pre("save" , async function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  
-  this.password = bcrypy.hash(this.password , 10)
-  next()
-})
+  this.password = bcrypy.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
- await bcrypy.compare(password, this.password)
-}
+  await bcrypy.compare(password, this.password);
+};
 
-userSchema.methods.gererateAccessToken = function (){}
-userSchema.methods.gererateRefreshToken = function (){}
+userSchema.methods.gererateAccessToken = function () {
+  return jwt.sign(
+    {
+    _id: this.id,
+    email: this.id,
+    username: this.username,
+    fullname: this.fullname,
+  } ,
+  process.env.ACCESS_TOKEN_SECRET ,
+  {
+    expiresIn : process.env.ACCESS_TOKEN_EXPIRY 
+  }
+);
+};
+userSchema.methods.gererateRefreshToken = function () {
+  return jwt.sign(
+    {
+    _id: this.id,
+    email: this.id,
+    username: this.username,
+    fullname: this.fullname,
+  } ,
+  process.env.REFRESH_TOKEN_SECRET ,
+  {
+    expiresIn : process.env.REFRESH_TOKEN_EXPIRY 
+  }
+);
+};
 
 export const User = mongoose.model("User", userSchema);
