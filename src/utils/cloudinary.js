be7745ac26fcf8cf4408c89fc116f333c8221e79
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-08 23:45:53 +0530

    add try catch

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,12 @@ cloudinary.config ( {
     api_key : process.env.CLOUDINARY_API_KEY, 
     api_secret : process.env.CLOUDINARY_API_SECRET
 }) ;
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
