570f850da1355531669e7b5c7e65010714cc811a
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-11 23:10:44 +0530

    server request send

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js" ;
 
 const registerUser = asyncHandler(async (req , res) => {
-    res.status(200).json({
-        message : "Ok"
-    })
+   // get user details from frontend
+   // validation check not empty 
+   // check if user already exists unique email username
+   // check for images and check for avatar 
+   // upload them to claoudianry
+   // delete uploaded files from server
+   // create user object - create entry in db 
+   // remove password and refresh token field from response
+   // check for user creation
+   // return response
+
+
+   const {fullname , username , email , password } = req.body
+   console.log("email: " , email ) ;
+
 } )
 
 export {
