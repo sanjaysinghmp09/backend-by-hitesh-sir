611f3be14e6fe8a329de78d4cb91d03e8df81aae
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-11 22:51:58 +0530

    server running

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 
 // routes declaration
-app.use("api/v1/users" , userRouter)
+app.use("/api/v1/users" , userRouter)
+
 
 export {app}
 
