f8ec11cd391d14c42889c91723494aaebcad2813
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-03-14 22:12:13 +0530

    ADD CONFIG

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,27 +1,21 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
-
-
-
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary i use "" v2.uploader.upload replace  cloudinary.uploader.upload
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary i use "" v2.uploader.upload replace  cloudinary.uploader.upload
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
-    }
-}
-
-
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
